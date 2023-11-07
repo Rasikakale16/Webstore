bd0071c0b8834cc1e77bd49ac77894ab52a95b0f
Rasika Kale <66539908+Rasikakale16@users.noreply.github.com>
2023-11-08 00:28:49 +0530

    Update chromeExtension.js

--- chromeExtension.js
+++ chromeExtension.js
@@ -1,10 +1,29 @@
+// const inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click",function(){
+//     console.log("Button clicked ");
+// })
+
+
 // function saveLead() {
 //     console.log("Button clicked");
 // }
 
+//creating two variables 
+//myleads - assigned to empty array
+//inputEl - assigned to text input field
 
-let inputBtn = document.getElementById("input-btn")
+let myleads = []
+const inputEl = document.getElementById("input-el")  //const cant be reassigned
+
+const inputBtn = document.getElementById("input-btn")
+
+// pushing the value "www.xyz.com" to myArray when the input button is clicked
+
 
 inputBtn.addEventListener("click",function(){
-    console.log("Button clicked from addEventListener");
+    //console.log("Button clicked ");
+    //myleads.push("www.xyz.com")
+    myleads.push(inputEl.value)
+    console.log(myleads);
 })
